bb0e2b0a720c212115d0af696dd9722eb103a085
Brandon Martinez <brandonam04@outlook.com>
2022-05-15 11:49:00 -0400

    Continue to make changes to navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,8 +1,17 @@
+import Link from 'next/link'
 export default function Navbar () {
   return(
-    <div class='main'>
-      <ul>
-        <li> <Link href=''> dialedIn</Link></li>
+    <div className='min-h-screen w-10'>
+      <ul className ='grid grid-flow-row list-none min-h-screen'>
+        <li className='my-0 object-top'>
+          <Link href=''><div className='w-10 h-10 bg-black'></div></Link>
+        </li> 
+        <li className='object-top my-0'>
+          <Link href=''><div className='w-10 h-10 bg-black'></div></Link>
+        </li> 
+        <li className='place-self-end my-0'>
+          <Link href=''><div className='w-10 h-10 m-0 bg-black'></div></Link>
+        </li> 
       </ul>
     </div>
   );
