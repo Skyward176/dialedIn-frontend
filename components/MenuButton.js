a2d1132d30a105074f6afb7def15df7a5ace97e7
Brandon Martinez <brandonam04@outlook.com>
2022-05-18 01:45:29 +0000

    Navbar edits

--- components/MenuButton.js
+++ components/MenuButton.js
@@ -1,16 +1,11 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useState } from 'react'
 export default function MenuButton(props) {
-    const [state, setState] = useState(props.clicked)
-    const onClickHandler = () => {
-        console.log('click event fired')
-        setState(!state)
-    }
-    if (state) {
+    if (props.navbarState === props.text) {
         return (
             <div
                 className='text-gray-50 bg-gray-800 rounded-lg flex place-items-center px-1 font-sans'
-                onClick={onClickHandler}
+                onClick={props.onClickHandler(props.text)}
             >
                 <FontAwesomeIcon className='h-6 m-4' icon={props.icon} />{' '}
                 <p className='text-lg text-gray-50'>{props.text}</p>
@@ -20,7 +15,7 @@ export default function MenuButton(props) {
         return (
             <div
                 className='bg-black flex place-items-center px-1 font-sans'
-                onClick={onClickHandler}
+                onClick={props.onClickHandler(props.text)}
             >
                 <FontAwesomeIcon
                     className='h-6 m-4 text-gray-50'
--- components/Navbar.js
+++ components/Navbar.js
@@ -4,41 +4,38 @@ import { faList } from '@fortawesome/free-solid-svg-icons'
 import { faGears } from '@fortawesome/free-solid-svg-icons'
 import MenuButton from '../components/MenuButton.js'
 import { useState } from 'react'
+
 export default function Navbar() {
     const [state, setState] = useState({
-        activeButton: 'Home'
+        'active-button': 'Home',
     })
+    const onClickHandler = (newActive) => {
+        setState(newActive)
+    }
+
     return (
         <div className='w-56 h-full bg-black shadow-lg absolute divide-y divide-gray-50'>
-            <div className="inline-flex my-3 w-full justify-center">
-                    <Link href='/'><p className='font-light font-sans text-4xl text-gray-50'>dialed<span className='text-green-300'>In</span></p></Link>
-                </div>
+            <div className='inline-flex my-3 w-full justify-center'>
+                <Link href='/'>
+                    <p className='font-light font-sans text-4xl text-gray-50'>
+                        dialed<span className='text-green-300'>In</span>
+                    </p>
+                </Link>
+            </div>
             <ul className='list-none'>
                 <li>
                     <Link href='/'>
-                        <MenuButton
-                            icon={faStopwatch}
-                            text='Home'
-                            clicked={true}
-                        />
+                        <MenuButton icon={faStopwatch} text='Home' onClickHandler={onClickHandler} />
                     </Link>
                 </li>
                 <li>
                     <Link href='/'>
-                        <MenuButton
-                            icon={faList}
-                            text='History'
-                            clicked={false}
-                        />
+                        <MenuButton icon={faList} text='History' clicked={false} />
                     </Link>
                 </li>
                 <li className='absolute bottom-0 w-full'>
                     <Link href='/'>
-                        <MenuButton
-                            icon={faGears}
-                            text='Settings'
-                            clicked={false}
-                        />
+                        <MenuButton icon={faGears} text='Settings' clicked={false} />
                     </Link>
                 </li>
             </ul>
