5af32781a52e4f9ce4209152fd864376b1fab0a2
Brandon Martinez <brandonam04@outlook.com>
2022-08-18 23:16:32 -0400

    Make get request to populate coffee selection

--- components/InputForm.js
+++ components/InputForm.js
@@ -2,18 +2,24 @@ import MenuButton from './MenuButton'
 import SubmitButton from './SubmitButton'
 import Select from 'react-select'
 import axios from 'axios'
-import { useEffect } from 'react'
+import { useEffect, useState} from 'react'
 
 const baseURL= 'http://localhost:8000'
 export function InputFormTop() {
-    const options = [{ value: 'coffeeid1', label: 'coffee 1' }]
+    const [options, setOptions] = useState(0)
     useEffect(() => {
-        axios.get(baseURL+'/coffees').then((response)=>{console.log(response)})
+        axios.get(baseURL+'/coffees').then((res)=>{
+            let data = res.data.results
+            return data
+        }).then((data) => {
+            console.log(data)
+            setOptions(data)    
+        }).catch((err) => console.log(err))
     }, []);
     return (
         <div>
             <input type='range' min='0' max='20' name='Mass In'></input>
-            <Select options={options} />
+            <Select options = {options} getOptionLabel={(options) => options['coffee_name']} getOptionValue={(options)=>options['coffee_name']}/>
         </div>
     )
 }
