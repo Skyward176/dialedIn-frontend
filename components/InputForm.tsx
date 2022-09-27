6f0ec96ec022be1acdaafee08432d50516916781
Brandon Martinez <brandonam04@outlook.com>
2022-09-27 18:05:54 -0400

    Add preflighting function for requests

--- components/InputForm.tsx
+++ components/InputForm.tsx
@@ -3,11 +3,44 @@ import Select from 'react-select'
 import axios from 'axios'
 import { useEffect, useState} from 'react'
 import {useCookies} from 'react-cookie'
+import {useTokenContext} from '../components/TokenContext'
+
+
 const baseURL= 'http://localhost:8000'
 export function InputFormTop(props) {
     const [options, setOptions] = useState(0)
     const [cookies, setCookies, removeCookies] = useCookies()
+
+    //const preflightRequest = useTokenContext()
+    const tokenPreflight = () => {
+        axios.post(
+            baseURL + '/token/validate', {
+                headers: {
+                    Authorization: `Bearer ${cookies['access']}`,
+                    'Content-Type': 'application/json',
+                }
+            }        
+        ).catch(function (error) {
+            if(error.response){
+                axios.post(
+                    baseURL + '/token/', {
+                        headers: {
+                            Authorization: `Bearer ${cookies['access']}`,
+                            'Content-Type': 'application/json',
+                        }
+                    }        
+                ).then(function (response){
+                    setCookies('access',response.data.access)
+                    setCookies('refresh',response.data.refresh)
+                    console.log('Tokens Refreshed')
+                })
+            } else {
+                console.log(error)
+            }
+        });
+    }
     useEffect(() => {
+        tokenPreflight()
         axios.get(baseURL+'/coffees', {
             headers: {
                 'Authorization': `Bearer ${cookies['access']}`,
---
+++ components/TokenContext.tsx
@@ -0,0 +1,43 @@
+import {useContext, createContext} from 'react'
+import axios from 'axios'
+import {useCookies} from 'react-cookie'
+
+const TokenValidateContext = createContext()
+const baseURL = 'localhost:8000'
+export function useTokenContext() {
+    return useContext(TokenValidateContext)
+}
+export function TokenValidateProvider({children}) {
+    const [cookies, setCookies, removeCookies] = useCookies()
+    const tokenPreflight = () => {
+        axios.post(
+            baseURL + '/token/validate', {
+                headers: {
+                    Authorization: `Bearer ${cookies['access']}`,
+                    'Content-Type': 'application/json',
+                }
+            }        
+        ).catch(function (error) {
+            if(error.response){
+                axios.post(
+                    baseURL + '/token/', {
+                        headers: {
+                            Authorization: `Bearer ${cookies['access']}`,
+                            'Content-Type': 'application/json',
+                        }
+                    }        
+                ).then(function (response){
+                    setCookies('access',response.data.access)
+                    setCookies('refresh',response.data.refresh)
+                })
+            } else {
+                console.log(error)
+            }
+        });
+    }
+    return(
+        <TokenValidateContext.Provider value = {tokenPreflight}>
+            {children}
+        </TokenValidateContext.Provider>
+    )
+}
